a44e4d1f8e4dda6cd117b06db7b4ced7ad453c9a
marcellimsuanon <marcellimsuanon@gmail.com>
2023-01-13 10:02:08 +0100

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <title>Finanza - Financial Services Website Template</title>
+    <title>PayDunYa-Transfer</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta name="csrf-token" content="{{ csrf_token() }}">
